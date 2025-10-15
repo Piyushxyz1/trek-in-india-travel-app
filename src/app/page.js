fe0085163987e3c2a0f6a592ac91a1a4317cd879
piyushxyz1 <piyushnegi033@gmail.com>
2025-10-15 15:24:16 +0530

    changes made

--- src/app/page.js
+++ src/app/page.js
@@ -2,38 +2,6 @@
 import Home from '@/components/Home'
 import React from 'react'
 
-// export const metadata = {
-//   title: "Home",
-//   robots: "noindex, nofollow", // 👈 prevents indexing
-// };
-
-export const metadata = {
-  title: "Trekking India – Your Adventure Starts Here",
-  description: "Explore Himalayan treks like Kedarkantha, Hampta Pass, and more – find your next trekking adventure!",
-  openGraph: {
-    title: "Trekking India – Your Adventure Starts Here",
-    description: "Explore Himalayan treks like Kedarkantha, Hampta Pass, and more – find your next trekking adventure!",
-    url: "https://trekkingindia.vercel.app",
-    siteName: "Trekking India",
-    images: [
-      {
-        url: "https://trekkingindia.vercel.app/images/home-og.jpg", // Use a real image path
-        width: 1200,
-        height: 630,
-        alt: "Trekking India",
-      },
-    ],
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Trekking India – Your Adventure Starts Here",
-    description: "Explore Himalayan treks like Kedarkantha, Hampta Pass, and more.",
-    images: ["https://trekkingindia.vercel.app/images/home-og.jpg"],
-  },
-};
-
-
 const page = () => {
   return (
     <div>
